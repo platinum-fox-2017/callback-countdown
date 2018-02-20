1749a7347f411fb930058eaa00db378b7d2880ea
agungcaproex <agung.caproex@gmail.com>
2018-02-20 14:09:19 +0700

    release 0

--- controllers.js
+++ controllers.js
@@ -2,6 +2,21 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let interval = setInterval(function(){
+    if(seconds < 1){
+      clearInterval(interval)
+    }
+    let minute = Math.floor(seconds / 60)
+    if(minute < 10){
+      minute = '0' + minute
+    }
+    let second = Math.floor(seconds % 60)
+    if(second < 10){
+      second = '0' + second
+    }
+    displayCountdown(minute, second)
+    seconds--
+  }, 1000)
 };
 
 module.exports = {
