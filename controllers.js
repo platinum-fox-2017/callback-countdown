4e686a5f99326dd29c4571d9e1c6bdc5aae6f775
adhiartaputra <adhiartaprabataputra@gmail.com>
2018-02-20 11:22:13 +0700

    Gong xi

--- controllers.js
+++ controllers.js
@@ -1,7 +1,28 @@
 const { displayCountdown } = require("./views");
 
-const timer = seconds => {
+const timer = (seconds) => {
   // Your code here...
+  let minutes = Math.floor(seconds / 60)
+  let newSecond = seconds % 60
+  let countdown = setInterval(
+    function () {
+    if(minutes < 10 && newSecond < 10) {
+      displayCountdown('0'+ minutes + ':' + '0' + newSecond)
+      newSecond--
+      if(newSecond < 0) {
+        minutes--
+        newSecond = 59
+      } else if(newSecond === 0 && minutes === 0) {
+        clearInterval(countdown)
+        displayCountdown('GONG XI FAT CHAI!!')
+      }
+    } else {
+      displayCountdown('0'+ minutes + ':' + newSecond)
+      newSecond--
+    }
+
+  },1000
+  )
 };
 
 module.exports = {
