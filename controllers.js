5f89ad9120662ec1b5f338e43291c816227de14d
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-20 13:02:40 +0700

    release 1

--- controllers.js
+++ controllers.js
@@ -1,7 +1,28 @@
-const { displayCountdown } = require("./views");
+const {
+  displayCountdown
+} = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let countdown = setInterval(() => {
+    displayCountdown(printSeconds(seconds))
+    seconds--
+    if (seconds < 0) {
+      clearInterval(countdown)
+    }
+  }, 1000)
+
+  function printSeconds(seconds) {
+    let minutes = Math.round(seconds / 60)
+    let secs = seconds % 60
+    if (minutes < 10 && secs < 10) {
+      return minutes = '0' + minutes + ' : ' + '0' + secs
+    }else if (minutes < 10) {
+      return console.log('0' + minutes + ' : ' + secs)
+    }else if (secs < 10) {
+      return console.log(minutes + ' : ' + '0' +secs);
+    }
+  }
 };
 
 module.exports = {
