0fad967c439354c266a23270a59f3e2448c38979
kevinhimawanhie <kevinhimawanhie@gmail.com>
2018-02-20 14:19:32 +0700

    Feedback

--- controllers.js
+++ controllers.js
@@ -1,9 +1,25 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let start = seconds
+  const countDown = setInterval(function(){ 
+    let hour = Math.floor(start / 3600), minute = Math.floor((start - (hour*3600))/60)
+    let detik = (start - (hour*3600)) - (minute*60)
+
+    if(Array.from(String(hour)).length === 1) hour = `0${hour}`
+
+    if(Array.from(String(minute)).length === 1) minute = `0${minute}`
+
+    if(Array.from(String(detik)).length === 1) detik = `0${detik}`
+
+    displayCountdown(`${hour} : ${minute} : ${detik}`)
+
+    start--
+    if(start === 0) clearInterval(countDown);
+
+  }, 1000);
 };
 
 module.exports = {
   timer
-};
+}
