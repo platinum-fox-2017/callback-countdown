9f770d143636c59282d47991cdc9b27597a66339
Ervan Adetya <vaanz.adetya@gmail.com>
2018-02-20 12:01:35 +0700

    First Commit

--- controllers.js
+++ controllers.js
@@ -1,8 +1,23 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
-};
+    let i =0;
+    let timer = setInterval(function(){
+      if(i>seconds) clearInterval(timer);
+      else {
+        displayCountdown(secondsToString(i));
+        i++
+      }
+    }, 1000);
+  };
+  
+const secondsToString = seconds => {
+    let secondsSTR = '';
+    let min = Math.floor(seconds/60);
+    let sec = seconds%60;
+    secondsSTR = (min<10 ? '0'+min : min) +":"+(sec<10 ? '0'+sec : sec);
+    return secondsSTR
+}
 
 module.exports = {
   timer
