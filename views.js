ed5c56143a7230ba14fccddd4bd9ccd0bdf6767b
root <irsyadpahlapi28@gmail.com>
2018-02-20 02:42:48 -0500

    done release 1

--- views.js
+++ views.js
@@ -1,11 +1,13 @@
 const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
+const supportsColor = require('supports-color');
+
 
 const displayCountdown = seconds => {
-  clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
-};
+    clear();
+    console.log(chalk.cyan(figlet.textSync("Countdown App")));
+    console.log(chalk.rgb(100, 100, 100)(figlet.textSync(seconds)));
+  };
 
 module.exports = { displayCountdown };
