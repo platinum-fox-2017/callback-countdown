545ffc12e6ff4f4cd619895596dcf50e88a20b1d
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-02-20 11:17:13 +0700

    release1

--- controllers.js
+++ controllers.js
@@ -1,8 +1,31 @@
 const { displayCountdown } = require("./views");
 
+
 const timer = seconds => {
-  // Your code here...
-};
+  let minute = seconds % 60
+  let hour = Math.floor(seconds/60)
+  var counter = setInterval(
+    function () {
+      if (minute < 10 && hour < 10) {
+        displayCountdown('0'+hour + ':' +'0'+ minute)
+        minute -= 1
+        if (minute < 0 && hour == 0) {
+          clearTimeout(counter)
+          displayCountdown('boom')
+        }
+        else if (minute < 0) {
+          hour -= 1
+          minute = 59
+        }
+      }
+      else {
+        displayCountdown('0'+hour + ':' + minute)
+        minute -= 1
+      }
+
+    },1000)
+}
+
 
 module.exports = {
   timer
