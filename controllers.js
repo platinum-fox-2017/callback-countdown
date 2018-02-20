4ea65eec9ffe8feeae86291cbe2fc878482e1d73
WikaSilo <wikanyaa@gmail.com>
2018-02-20 11:51:47 +0700

    WikaSilo

--- controllers.js
+++ controllers.js
@@ -2,7 +2,27 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  // let current = seconds
+  let current = setInterval(function(){
+    if(seconds < 0) {
+      clearInterval(current)
+    } else {
+
+      var mind = seconds % (60 * 60);
+      var min = Math.floor(mind / 60);
+      if(min<10) min = '0'+min
+
+      var secd = mind % 60;
+      var sec = Math.ceil(secd);
+      if(sec<10) sec = '0'+sec
+
+      displayCountdown(`${min} : ${sec}`);
+      seconds--
+    }
+  }, 1000);
 };
+
+
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -2,10 +2,13 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
+// const timer = require("./controller.js")
+
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.green(figlet.textSync("Countdown App")));
   // Your code here...
+  console.log(chalk.cyan(figlet.textSync(seconds)));
 };
 
 module.exports = { displayCountdown };
